935b34f8d948ff626659d8f0400dea390a0cecab
JuliaBessonova <bessonova.jul@gmail.com>
2026-09-08 21:59:54 +0300

    websocket exercises

---
+++ websocket_users_client.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+
+
+async def client():
+    uri = "ws://localhost:8765"  # Адрес сервера
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("Привет, сервер!")  # Отправляем сообщение
+
+        for _ in range(5):
+            message = await websocket.recv()
+            print(message)
+
+
+asyncio.run(client())
